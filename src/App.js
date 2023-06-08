cc703c2ccd0d0f677d29612dc1cc925ce8aa0dfc
asif <asif.h.jahid@siarasolution.com>
2023-06-08 20:38:33 +0600

    code structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import logo from "./logo.svg";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WahtGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Modern React !</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WahtGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WhatGPT3() {
+  return <div>WhatGPT3</div>;
+}
